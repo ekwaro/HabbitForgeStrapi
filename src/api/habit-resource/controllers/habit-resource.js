503a9dff0bca6966e16133c47eda6dd05c07952c
SaulWeaponz <saulweaponz@gmail.com>
2025-06-29 21:58:37 +0300

    Add habit resource files

---
+++ src/api/habit-resource/controllers/habit-resource.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * habit-resource controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::habit-resource.habit-resource');
