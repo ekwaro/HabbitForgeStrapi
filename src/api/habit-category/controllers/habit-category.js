c5b4364e5b3dd3dca982883ad3ed22d3c0a11af8
SaulWeaponz <saulweaponz@gmail.com>
2025-06-29 17:49:49 +0300

    Add habit-category settings

---
+++ src/api/habit-category/controllers/habit-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * habit-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::habit-category.habit-category');
---
+++ src/api/habit-category/services/habit-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * habit-category service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::habit-category.habit-category');
