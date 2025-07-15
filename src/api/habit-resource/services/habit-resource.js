2c4198045374bffc06be587d642712a5c3c33cc2
SaulWeaponz <saulweaponz@gmail.com>
2025-07-15 11:13:31 +0300

    Add tip-resource folder

--- src/api/habit-resource/services/habit-resource.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * habit-resource service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::habit-resource.habit-resource');
